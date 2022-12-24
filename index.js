e8df9f061ee580256e39ce7d9e98dd05b00849c1
kovacsv <viktorkovacs@gmail.com>
2022-12-24 18:07:20 +0100

    Add npm module and code.

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import * as childProcess from 'child_process';
+
+let pythonExecutable = 'python';
+if (process.platform !== 'win32') {
+    pythonExecutable = 'python3';
+}
+
+let args = process.argv.slice (2);
+let result = childProcess.spawnSync (pythonExecutable, args, {
+    stdio: "inherit"
+});
+
+process.exit (result.status);
